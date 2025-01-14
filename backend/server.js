6dadf91d1d2b4830206770acf103dfc677e19862
Matheus Ferreira De Araujo <105127162+MTSAraujo29@users.noreply.github.com>
2025-01-13 23:54:05 -0300

    2 tentativa

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,13 @@ app.get('/', (req, res) => {
     res.redirect('/smartutilities');
 });
 
+// Adicione uma rota de healthcheck
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
